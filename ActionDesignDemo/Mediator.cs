b0c49439f799ece6d3e072d84575c594f4f085bf
帅 李 <2363502880@qq.com>
2023-02-26 10:58:48 +0800

    mediator and observere

---
+++ ActionDesignDemo/Mediator.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ActionDesignDemo
+{
+    /// <summary>
+    /// 解耦系统内各个对象之间的关联关系（mediator随着越来越庞大，单个对象可能通知莫一部分对象，大量ifelse随着而来）
+    /// </summary>
+    public abstract class Mediator
+    {
+        List<Element> elements = new();
+        public abstract void Modify(Element send,Object message);
+        public virtual void AddElement(Element element)
+        {
+            elements.Add(element);
+        }
+    }
+    public class ConocreteMediator : Mediator
+    {
+
+        private CutMenuItem1 CutMenuItem1;
+        private TextArea1 textArea1;
+        private ClipBoard1 clipBoard1;
+        private ToolBarButton1 toolBarButton1;
+        public override void Modify(Element sender,object message)
+        {
+            //假设文本右键菜单后都是自动的
+            if(sender == CutMenuItem1)
+            {
+                textArea1.SelectedText();
+            }else if(sender == textArea1)
+            {
+                //通知剪切板复制值
+                clipBoard1.OnChange(message);
+            }else if(sender == clipBoard1)
+            {
+                toolBarButton1.OnChange(message);
+            }
+        }
+    }
+
+    public class Client
+    {
+        public static void Main()
+        {
+            var mediator = new ConocreteMediator();
+            var CutMenuItem1 = new CutMenuItem1(mediator);
+            var TextArea1 = new CutMenuItem1(mediator);
+            var ClipBoard1 = new CutMenuItem1(mediator);
+            var ToolBarButton1 = new CutMenuItem1(mediator);
+
+            CutMenuItem1.Click();
+            CutMenuItem1.OnChange();
+
+        }
+    }
+
+    #region 初始版：模拟文本全选重新粘贴新内容
+    class CutMenuItem
+    {
+        TextArea TextArea;
+        ClipBoard ClipBoard;
+        ToolBarButton toolBarButton;
+
+        public void Click()
+        {
+            string text = TextArea.SelectText();
+            ClipBoard.SetData(text);
+
+        }
+    }
+    class TextArea 
+    {
+        public string SelectText()
+        {
+            return "上次复制的文本";
+        }
+    }
+    class ClipBoard 
+    {
+        string data = string.Empty;
+        public void SetData(string txt) { this.data= txt; }
+    }
+    class ToolBarButton { }
+    #endregion
+    #region 中介者
+
+    public abstract class Element
+    {
+        Mediator mediator;
+        public Element(Mediator mediator)
+        {
+            this.mediator = mediator;
+            this.mediator.AddElement(this);
+        }
+        public abstract void OnChange(object Message);
+    }
+    class CutMenuItem1: Element
+    {
+        Mediator mediator;
+        private string cutText;
+        public CutMenuItem1(Mediator mediator) : base(mediator)
+        {
+        }
+
+        /// <summary>
+        /// 点击右键
+        /// </summary>
+        public void Click()
+        {
+            cutText = "获取了文本";
+        }
+        public override void OnChange(object message)
+        {
+            //剪切的文本内容
+            mediator.Modify(this, cutText);
+        }
+    }
+    class TextArea1:Element
+    {
+        Mediator mediator;
+        private string text;
+        public TextArea1(Mediator mediator) : base(mediator)
+        {
+            text = "";
+            this.mediator = mediator;
+        }
+        public void SelectedText()
+        {
+            this.OnChange("我是选中的文本");
+        }
+
+        public override void OnChange(object message)
+        {
+            Console.WriteLine("已选择文本");
+            mediator.Modify(this,text);
+        }
+    }
+    class ClipBoard1 : Element
+    {
+        Mediator mediator;
+        string data = string.Empty;
+
+        public ClipBoard1(Mediator mediator) : base(mediator)
+        {
+        }
+
+        public override void OnChange(object message)
+        {
+            mediator.Modify(this,message);
+        }
+
+        public void SetData(string txt) { this.data = txt; }
+    }
+    class ToolBarButton1 : Element
+    {
+        Mediator mediator;
+        public ToolBarButton1(Mediator mediator) : base(mediator)
+        {
+        }
+
+        public void EnablePaste()
+        {
+            this.OnChange("");
+        }
+
+        public override void OnChange(object message)
+        {
+            mediator.Modify(this,message);
+        }
+    }
+    #endregion
+}
---
+++ ActionDesignDemo/Observer.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionDesignDemo
+{
+    /// <summary>
+    /// 观察者（事件）设计模式
+    /// </summary>
+    internal class Observer
+    {
+    }
+    #region 初始设计
+    public class BankAccount
+    {
+        private Emailer emailer;
+        private Mobiler mobiler;
+        public void WithDraw(int money)
+        {
+            emailer.SendEmail("236350...qq.com");
+            mobiler.SendMobiler("166386...");
+        }
+    }
+    public class Emailer
+    {
+        public void SendEmail(string email)
+        {
+            ///....
+        }
+    }
+    public class Mobiler
+    {
+        public void SendMobiler(string phone)
+        {
+            ///...
+        }
+    }
+    #endregion
+
+    #region 观察者1.0
+
+    /// <summary>
+    /// 当只有一个消息通知行为时候，且BankAccount1 内部稳定时候
+    /// </summary>
+    public interface IAccountObserver
+    {
+        public void UpdateMessage(UserAccountArgs args);
+    }
+    public class UserAccountArgs
+    {
+        public string email { get; set; }
+        public string phone { get; set; }
+    }
+
+    public class BankAccount1
+    {
+        IEnumerable<IAccountObserver> accountObservers;
+        public void WithDraw(UserAccountArgs args)
+        {
+            foreach (var item in accountObservers)
+            {
+                item.UpdateMessage(args);
+            }
+        }
+    }
+    public class Emailer1 : IAccountObserver
+    {
+        public void UpdateMessage(UserAccountArgs args)
+        {
+            //更新通知
+            //args.email...
+        }
+    }
+    public class Mobiler1 : IAccountObserver
+    {
+        public void UpdateMessage(UserAccountArgs args)
+        {
+            //更新通知
+            //args.phone...
+        }
+    }
+
+    #endregion
+
+    #region 观察者2.0
+
+    public abstract class Subject
+    {
+        protected List<IAccountObserver> accountObservers;
+        protected virtual void Notify(UserAccountArgs args)
+        {
+            foreach (IAccountObserver observer in accountObservers)
+            {
+                observer.UpdateMessage(args);
+            }
+        }
+        public void AddObserver(IAccountObserver observer)
+        {
+            accountObservers.Add(observer);
+        }
+        public void RemoveObserver(IAccountObserver observer)
+        {
+            accountObservers.Remove(observer);
+        }
+    }
+
+    public class BankAccount2: Subject
+    {
+        public void WithDraw(UserAccountArgs args)
+        {
+            AddObserver(new Mobiler1());
+            AddObserver(new Emailer1());
+            this.Notify(args);
+        }
+    }
+
+    #endregion
+
+    #region 事件中的观察者（更加松耦合）
+
+    public class App 
+    {
+        public static void Main()
+        {
+            BankAccountE bankAccountE = new BankAccountE();
+            EmailerE emailerE = new EmailerE();
+            bankAccountE.AccountChange += new AccountChangeEventHander(emailerE.Update);
+            bankAccountE.WithDraw(123);
+        }
+    }
+
+    public delegate void AccountChangeEventHander(object sender, UserAccountArgs args);
+    public class BankAccountE
+    {
+        //内部就是观察者2.0的链表(遍历行为)和add以及remove
+        public event AccountChangeEventHander AccountChange;
+        public void WithDraw(int money)
+        {
+            UserAccountArgs args = new UserAccountArgs();
+            this.OnAccountChange(args);
+        }
+        protected virtual void OnAccountChange(UserAccountArgs args)
+        {
+            if(AccountChange != null)
+            {
+                AccountChange(this,args);
+            }
+        }
+    }
+
+    public class EmailerE
+    {
+        public void Update(object sender, UserAccountArgs args)
+        {
+            //...
+        }
+    }
+
+    #endregion
+}
